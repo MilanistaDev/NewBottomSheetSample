e27b8a115f12245d31c21b834810b73ce5da4c97
Takuya Aso <milanista224@gmail.com>
2021-06-10 21:05:20 +0900

    AnimalListを表示する処理

--- NewBottomSheet/ViewController.swift
+++ NewBottomSheet/ViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var animalLabel: UILabel!
+    var animalList: AnimalViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +20,18 @@ class ViewController: UIViewController {
 
     private func setUpUI() {
         animalLabel.text = "❓"
+        let sb = UIStoryboard(name: "AnimalList", bundle: nil)
+        animalList = sb.instantiateInitialViewController()!
     }
 
     @IBAction func buttonDidTap(_ sender: Any) {
         
+        animalList.modalPresentationStyle = .popover
+        if let sheet = animalList.popoverPresentationController?.adaptiveSheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.prefersGrabberVisible = true
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+        }
+        present(animalList, animated: true)
     }
 }
